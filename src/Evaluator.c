Teknomancer/nopf

commit 05bd1f3ee09272b2a78644dc03adb10ce5f57845

Evaluator: Fix crash when expression has an opening paranthesis when a previous atom exists but is not a functor. e.g: "1 + 2 ()"

src/Evaluator.c
@@ -1093,6 +1093,15 @@ int EvaluatorParse(PEVALUATOR pEval, const char *pszExpr)
             PCOPERATOR pOperator = pAtom->u.pOperator;
             if (OperatorIsOpenParenthesis(pOperator))
             {
+                if (   pPreviousAtom
+                    && !AtomIsFunctor(pPreviousAtom))
+                {
+                    DEBUGPRINTF(("Paranthesis begins when previous atom is not a functor!\n"));
+                    MemFree(pAtom);
+                    EvaluatorCleanUp(pEval, &Stack);
+                    return RERR_EXPRESSION_INVALID;
+                }
+
                 /*
                  * Open parenthesis.
                  */

commit 75af06126ccd09e025b78ca8dd2cbd3b3f753e67

Evalator: Commit 05bd1f3ee09272b2a78644dc03adb10ce5f57845 broke expressions like "((1+2))". Back out!

src/Evaluator.c
@@ -1093,15 +1093,6 @@ int EvaluatorParse(PEVALUATOR pEval, const char *pszExpr)
             PCOPERATOR pOperator = pAtom->u.pOperator;
             if (OperatorIsOpenParenthesis(pOperator))
             {
-                if (   pPreviousAtom
-                    && !AtomIsFunctor(pPreviousAtom))
-                {
-                    DEBUGPRINTF(("Paranthesis begins when previous atom is not a functor!\n"));
-                    MemFree(pAtom);
-                    EvaluatorCleanUp(pEval, &Stack);
-                    return RERR_EXPRESSION_INVALID;
-                }
-
                 /*
                  * Open parenthesis.
                  */